633500d9030c3b306c0feaf013d474600ebac46a
Rithikakalaimani <123987662+Rithikakalaimani@users.noreply.github.com>
2024-06-07 21:11:35 +0530

    problems : arrays

--- Arrays/Remove_Duplicates.js
+++ Arrays/Remove_Duplicates.js
@@ -9,10 +9,22 @@ function removeDuplicates(arr) {
   return i + 1;
 }
 
+function removeDuplicate(arr) {
+  let set = new Set(arr);
+  let uniqueArr = Array.from(set);
+  for (let i = 0; i < uniqueArr.length; i++) {
+    arr[i] = uniqueArr[i];
+  }
+  return uniqueArr.length;
+}
 const arr = [1, 1, 2, 2, 2, 3, 3];
 const k = removeDuplicates(arr);
-
+const l = removeDuplicate(arr);
 console.log("The array after removing duplicate elements is:");
 for (let i = 0; i < k; i++) {
+  console.log(arr[i]);
+}
+console.log("The array after removing duplicate elements is:");
+for (let i = 0; i < l; i++) {
   console.log(arr[i]);
 }
