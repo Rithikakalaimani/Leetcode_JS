706ffbad03670d75c1b4c3da850582c4b525b4f5
Rithikakalaimani <123987662+Rithikakalaimani@users.noreply.github.com>
2024-06-12 23:36:00 +0530

    problems : arrays

--- Arrays/Left_ROtate_ByOne.js
+++ Arrays/Left_ROtate_ByOne.js
@@ -3,6 +3,7 @@ function solve1(arr, n) {
   for (let i = 1; i < n; i++) {
     temp[i - 1] = arr[i];
   }
+  
   temp[n - 1] = arr[0];
   for (let i = 0; i < n; i++) {
     console.log(temp[i] + " ");
